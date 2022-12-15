9b1e0eb9b27c8860d27167f8d24941222a961640
KamilSkrzynski <kamilskrzynski95@gmail.com>
2022-12-15 15:17:54 +0100

    Initial commit

---
+++ OpenAI Image Generator/OpenAI_Image_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAI_Image_GeneratorApp.swift
+//  OpenAI Image Generator
+//
+//  Created by Kamil Skrzyński on 15/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct OpenAI_Image_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
