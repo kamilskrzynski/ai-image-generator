02e37ab1907fc98ff050336016a79a3cd92c6c90
KamilSkrzynski <kamilskrzynski95@gmail.com>
2023-01-02 12:02:08 +0100

    Full UI

--- OpenAI Image Generator/ContentView.swift
+++ OpenAI Image Generator/ContentView.swift
@@ -12,29 +12,63 @@ struct ContentView: View {
     @StateObject private var viewModel = ViewModel()
     @State var prompt: String = ""
     @State var generatedImage: UIImage?
+    @State var isLoading: Bool = false
     
     var body: some View {
-        VStack {
-            Spacer()
-            if let generatedImage = generatedImage {
-                Image(uiImage: generatedImage)
-                    .resizable()
-                    .frame(width: 250, height: 250)
-            } else {
-                Text("You need to provide prompt to generate an image")
-            }
-            Spacer()
-            TextField("Type prompt here...", text: $prompt)
-            Button("Generate") {
-                Task {
-                    generatedImage =  await viewModel.generateImage(from: prompt)
-                }
-            }
-            .onAppear {
-                viewModel.setup()
-            }
-            .padding()
+        ZStack {
+             if isLoading {
+                ProgressView()
+                     .tint(.white)
+                     .frame(maxWidth: .infinity, maxHeight: .infinity)
+                     .background(Color("backgroundColor"))
+             } else {
+                 VStack {
+                     Text("DALL-E IMAGE GENERATOR")
+                         .foregroundColor(.white.opacity(0.8))
+                         .font(.title2)
+                         .bold()
+                         .offset(y: 10)
+                     Spacer()
+                     if let generatedImage = generatedImage {
+                         Image(uiImage: generatedImage)
+                             .resizable()
+                             .frame(width: 250, height: 250)
+                     } else {
+                         Image("placeholderImage")
+                             .resizable()
+                             .frame(width: 250, height: 250)
+                             .opacity(0.5)
+                     }
+                     Spacer()
+                         Text("ENTER YOUR PROMPT BELOW")
+                             .foregroundColor(.white.opacity(0.6))
+                             .font(.caption2.bold())
+                         TextField("", text: $prompt)
+                         .padding()
+                         .font(.caption)
+                         .foregroundColor(.white.opacity(0.5))
+                             .background(Color("textFieldColor"))
+                             .cornerRadius(5)
+                     .padding(.horizontal)
+                     
+                     Button("Generate") {
+                         Task {
+                             isLoading = true
+                             generatedImage = await viewModel.generateImage(from: prompt)
+                             isLoading = false
+                         }
+                     }
+                     .foregroundColor(.white)
+                     .buttonStyle(.borderedProminent)
+                     .tint(.primary)
+                     .onAppear {
+                         viewModel.setup()
+                     }
+                     .padding()
+                 }
+             }
         }
+        .background(Color("backgroundColor"))
     }
 }
 
